d78066fea8bf905db0b0d9787ab46d8a57fc5936
Shana Wiltshire <shanawiltshire@gmail.com>
2019-10-26 00:44:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def supermario_brothers = mario
-  puts phrase
   phrase = "It's-a me, Mario!"
   $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
@@ -12,3 +11,4 @@ end
 def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
+end
